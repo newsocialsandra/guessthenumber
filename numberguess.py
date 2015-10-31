aca562dac71a618946a01f70cc4bb03c4b95c8fc
newsocialsandra <sandra.e.b.svensson@gmail.com>
2015-10-31 17:29:02 +0100

    Added while loop + if/elif statements

--- numberguess.py
+++ numberguess.py
@@ -1,9 +1,32 @@
 from random import randint
 
+guessesTaken = 0
+
 number = (randint(0,100))
 
-guess = raw_input("Guess a number between 0 and 100")
+print "I'm thinking of a number between 0 and 100"
+
+while guessesTaken < 6:
+	print "Take a guess!"
+	guess = raw_input()
+	guess = int(guess)
+
+	guessesTaken +=1
+
+	if guess < number:
+		print "Your guess is too low."
+
+	elif guess > number:
+		print "Your guess is too high."
+
+	elif guess == number:
+		break
+
 if guess == number:
-    print "WITCH!!"
-else:
-	print "Nope, wrong guess."
+	guessesTaken = str(guessesTaken)
+	print "Good job! You guessed my number in" + guessesTaken + " guesses!"
+
+elif guess != number:
+	number = str(number)
+	print "Nope, the number I was thinking of was" + number 
+
